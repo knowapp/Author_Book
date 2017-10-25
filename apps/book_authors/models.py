41bf2d1d9a2e9eeeb00611acb9e9c4a1961638f2
Melvin Patterson <mel-mbp17@Mel-MBP17-2.local>
2017-10-25 15:09:35 -0700

    author_book

---
+++ apps/book_authors/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    notes = models.TextField(max_length=1000, null=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    def __repr__(self):
+        return "object: {}, {}, {}, {}".format(self.first_name, self.last_name, self.email, self.notes)
+
+class Book(models.Model):
+    authors = models.ManyToManyField(Author, related_name="read")
+    name = models.CharField(max_length=255)
+    desc = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    def __repr__(self):
+        return "object: {}, {}".format(self.name, self.desc)
